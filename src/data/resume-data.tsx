337f468afc5439f62f73df5409d921beea4345d5
lilpacy <revivedtomorrow@gmail.com>
2024-03-31 05:34:07 +0900

    不要なデータを削除

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -3,9 +3,6 @@ import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
   name: "Pacy",
-  initials: "LP",
-  location: "東京, 日本",
-  locationLink: "https://www.google.com/maps/place/Tokyo",
   about: "フルスタックエンジニア",
   summary:
     "神戸大学在学中に個人事業で生計を立てる。その後、Donutsバンコク支社でRubyメインのフルスタックエンジニアとしてインターン勤務。2019年にFringe81株式会社にエンジニアとして新卒入社。HRTech事業フロントエンドを純粋関数型言語Elmで保守運用、新規開発。2020年に広告事業へ異動し、広告配信システムのログ集計基盤責任者としてScalaでサーバーサイド/インフラの保守運用と新規開発。2021年6月にCryptoGames.incにフルスタックエンジニアとして参加し、同年11月にNFT事業開発リードに就任。2022年7月に同社のCTOに就任。現在はNuxt.js/Next.jsを用いたフロントエンド開発、TypeScript/Go/Pythonなどを用いたサーバーサイド開発、Solidityを用いたスマートコントラクト開発などを行っている。",
