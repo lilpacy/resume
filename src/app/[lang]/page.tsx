0a21fc3b2177a1355647693a82ed6c1c3e97c2b3
lilpacy <revivedtomorrow@gmail.com>
2024-03-31 14:56:33 +0900

    変数名リファクタ

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -6,7 +6,7 @@ import { Metadata } from "next";
 import { Section } from "@/components/ui/section";
 import { GlobeIcon, MailIcon, PhoneIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { RESUME_DATA as _RESUME_DATA } from "@/constants";
+import { constants } from "@/constants";
 import { ProjectCard } from "@/components/project-card";
 import { Language, getDictionary } from "./dictionaries";
 
@@ -17,8 +17,8 @@ type Props = {
 };
 
 export const metadata: Metadata = {
-  title: `${_RESUME_DATA.name} | ${_RESUME_DATA.about}`,
-  description: _RESUME_DATA.summary,
+  title: `${constants.resume.name} | ${constants.resume.about}`,
+  description: constants.resume.summary,
 };
 
 export async function generateStaticParams() {
@@ -28,52 +28,51 @@ export async function generateStaticParams() {
 export default async function Page({ params }: Props) {
   console.log({ params });
   const dict = await getDictionary(params.lang);
-  const { RESUME_DATA } = dict;
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
         <div className="flex items-center justify-between">
           <div className="flex-1 space-y-1.5">
-            <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
+            <h1 className="text-2xl font-bold">{dict.resume.name}</h1>
             <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground">
-              {RESUME_DATA.about}
+              {dict.resume.about}
             </p>
             <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
               <a
                 className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
-                href={RESUME_DATA.locationLink}
+                href={dict.resume.locationLink}
                 target="_blank"
               >
                 <GlobeIcon className="size-3" />
-                {RESUME_DATA.location}
+                {dict.resume.location}
               </a>
             </p>
             <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
-              {RESUME_DATA.contact.email ? (
+              {dict.resume.contact.email ? (
                 <Button
                   className="size-8"
                   variant="outline"
                   size="icon"
                   asChild
                 >
-                  <a href={`mailto:${RESUME_DATA.contact.email}`}>
+                  <a href={`mailto:${dict.resume.contact.email}`}>
                     <MailIcon className="size-4" />
                   </a>
                 </Button>
               ) : null}
-              {RESUME_DATA.contact.tel ? (
+              {dict.resume.contact.tel ? (
                 <Button
                   className="size-8"
                   variant="outline"
                   size="icon"
                   asChild
                 >
-                  <a href={`tel:${RESUME_DATA.contact.tel}`}>
+                  <a href={`tel:${dict.resume.contact.tel}`}>
                     <PhoneIcon className="size-4" />
                   </a>
                 </Button>
               ) : null}
-              {_RESUME_DATA.contact.social.map((social) => (
+              {constants.resume.contact.social.map((social) => (
                 <Button
                   key={social.name}
                   className="size-8"
@@ -88,33 +87,36 @@ export default async function Page({ params }: Props) {
               ))}
             </div>
             <div className="hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex">
-              {RESUME_DATA.contact.email ? (
-                <a href={`mailto:${RESUME_DATA.contact.email}`}>
-                  <span className="underline">{RESUME_DATA.contact.email}</span>
+              {dict.resume.contact.email ? (
+                <a href={`mailto:${dict.resume.contact.email}`}>
+                  <span className="underline">{dict.resume.contact.email}</span>
                 </a>
               ) : null}
-              {RESUME_DATA.contact.tel ? (
-                <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                  <span className="underline">{RESUME_DATA.contact.tel}</span>
+              {dict.resume.contact.tel ? (
+                <a href={`tel:${dict.resume.contact.tel}`}>
+                  <span className="underline">{dict.resume.contact.tel}</span>
                 </a>
               ) : null}
             </div>
           </div>
 
           <Avatar className="size-28">
-            <AvatarImage alt={_RESUME_DATA.name} src={_RESUME_DATA.avatarUrl} />
-            <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
+            <AvatarImage
+              alt={constants.resume.name}
+              src={constants.resume.avatarUrl}
+            />
+            <AvatarFallback>{dict.resume.initials}</AvatarFallback>
           </Avatar>
         </div>
         <Section>
           <h2 className="text-xl font-bold">About</h2>
           <p className="text-pretty font-mono text-sm text-muted-foreground">
-            {RESUME_DATA.summary}
+            {dict.resume.summary}
           </p>
         </Section>
         <Section>
           <h2 className="text-xl font-bold">Work Experience</h2>
-          {RESUME_DATA.work.map((work) => {
+          {dict.resume.work.map((work) => {
             return (
               <Card key={work.company}>
                 <CardHeader>
@@ -154,7 +156,7 @@ export default async function Page({ params }: Props) {
         </Section>
         <Section>
           <h2 className="text-xl font-bold">Education</h2>
-          {RESUME_DATA.education.map((education) => {
+          {dict.resume.education.map((education) => {
             return (
               <Card key={education.school}>
                 <CardHeader>
@@ -175,7 +177,7 @@ export default async function Page({ params }: Props) {
         <Section>
           <h2 className="text-xl font-bold">Skills</h2>
           <div className="flex flex-wrap gap-1">
-            {RESUME_DATA.skills.map((skill) => {
+            {dict.resume.skills.map((skill) => {
               return <Badge key={skill}>{skill}</Badge>;
             })}
           </div>
@@ -184,7 +186,7 @@ export default async function Page({ params }: Props) {
         <Section className="print-force-new-page scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
-            {RESUME_DATA.projects.map((project) => {
+            {dict.resume.projects.map((project) => {
               return (
                 <ProjectCard
                   key={project.title}
@@ -202,10 +204,10 @@ export default async function Page({ params }: Props) {
       <CommandMenu
         links={[
           {
-            url: RESUME_DATA.personalWebsiteUrl,
+            url: dict.resume.personalWebsiteUrl,
             title: "Personal Website",
           },
-          ..._RESUME_DATA.contact.social.map((socialMediaLink) => ({
+          ...constants.resume.contact.social.map((socialMediaLink) => ({
             url: socialMediaLink.url,
             title: socialMediaLink.name,
           })),
--- src/constants.tsx
+++ src/constants.tsx
@@ -1,31 +1,33 @@
 import { PixelPacy } from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
-export const RESUME_DATA = {
-  name: "Pacy",
-  about: "フルスタックエンジニア",
-  summary:
-    "神戸大学在学中に個人事業で生計を立てる。その後、Donutsバンコク支社でRubyメインのフルスタックエンジニアとしてインターン勤務。2019年にFringe81株式会社にエンジニアとして新卒入社。HRTech事業フロントエンドを純粋関数型言語Elmで保守運用、新規開発。2020年に広告事業へ異動し、広告配信システムのログ集計基盤責任者としてScalaでサーバーサイド/インフラの保守運用と新規開発。2021年6月にCryptoGames.incにフルスタックエンジニアとして参加し、同年11月にNFT事業開発リードに就任。2022年7月に同社のCTOに就任。現在はNuxt.js/Next.jsを用いたフロントエンド開発、TypeScript/Go/Pythonなどを用いたサーバーサイド開発、Solidityを用いたスマートコントラクト開発などを行っている。",
-  avatarUrl: PixelPacy.src,
-  contact: {
-    email: "",
-    tel: "",
-    social: [
-      {
-        name: "GitHub",
-        url: "https://github.com/lilpacy",
-        icon: GitHubIcon,
-      },
-      {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/tomohiro-seto-00740a11a/",
-        icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/0xpacy",
-        icon: XIcon,
-      },
-    ],
+export const constants = {
+  resume: {
+    name: "Pacy",
+    about: "フルスタックエンジニア",
+    summary:
+      "神戸大学在学中に個人事業で生計を立てる。その後、Donutsバンコク支社でRubyメインのフルスタックエンジニアとしてインターン勤務。2019年にFringe81株式会社にエンジニアとして新卒入社。HRTech事業フロントエンドを純粋関数型言語Elmで保守運用、新規開発。2020年に広告事業へ異動し、広告配信システムのログ集計基盤責任者としてScalaでサーバーサイド/インフラの保守運用と新規開発。2021年6月にCryptoGames.incにフルスタックエンジニアとして参加し、同年11月にNFT事業開発リードに就任。2022年7月に同社のCTOに就任。現在はNuxt.js/Next.jsを用いたフロントエンド開発、TypeScript/Go/Pythonなどを用いたサーバーサイド開発、Solidityを用いたスマートコントラクト開発などを行っている。",
+    avatarUrl: PixelPacy.src,
+    contact: {
+      email: "",
+      tel: "",
+      social: [
+        {
+          name: "GitHub",
+          url: "https://github.com/lilpacy",
+          icon: GitHubIcon,
+        },
+        {
+          name: "LinkedIn",
+          url: "https://www.linkedin.com/in/tomohiro-seto-00740a11a/",
+          icon: LinkedInIcon,
+        },
+        {
+          name: "X",
+          url: "https://x.com/0xpacy",
+          icon: XIcon,
+        },
+      ],
+    },
   },
 } as const;
